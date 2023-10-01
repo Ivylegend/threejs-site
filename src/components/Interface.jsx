1d8014d1bad628541a7a8c2adf81c37d065b1f0c
Ivylegend <victoryicha2@gmail.com>
2023-10-01 07:35:52 +0100

    my commit

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -32,10 +32,6 @@ export const Interface = () => {
     <div className="flex flex-col items-center w-screen">
       <AboutSection />
       <SkillsSection />
-      <Section>
-        <h1>Projects</h1>
-      </Section>
-      <ContactSection />
     </div>
   );
 };
@@ -46,10 +42,10 @@ const AboutSection = () => {
       <h1 className="text-6xl font-extrabold leading-snug">
         Hi, I'm
         <br />
-        <span className="bg-white px-1 italic">Wawa Sensei</span>
+        <span className="bg-white px-1 italic">Icha Victory</span>
       </h1>
       <motion.p
-        className="text-lg text-gray-600 mt-4"
+        className="text-lg text-black-600 mt-4"
         initial={{
           opacity: 0,
           y: 25,
@@ -63,9 +59,9 @@ const AboutSection = () => {
           delay: 1.5,
         }}
       >
-        I make YouTube videos to help developers
+        I create amazing websites for clients
         <br />
-        learn how to build 3D apps
+        Also learning how to build 3D apps
       </motion.p>
       <motion.button
         className={`bg-indigo-600 text-white py-4 px-8 
@@ -92,7 +88,7 @@ const AboutSection = () => {
 const skills = [
   {
     title: "Threejs / React Three Fiber",
-    level: 80,
+    level: 30,
   },
   {
     title: "React / React Native",
@@ -113,15 +109,15 @@ const skills = [
 ];
 const languages = [
   {
-    title: "🇫🇷 French",
+    title: "🇺🇸 English",
     level: 100,
   },
   {
-    title: "🇺🇸 English",
+    title: "NG Yoruba",
     level: 80,
   },
   {
-    title: "🇯🇵 Japanese",
+    title: "FR French",
     level: 20,
   },
 ];
@@ -219,53 +215,6 @@ const SkillsSection = () => {
           </div>
         </div>
       </motion.div>
-    </Section>
-  );
-};
-
-const ContactSection = () => {
-  return (
-    <Section>
-      <h2 className="text-5xl font-bold">Contact me</h2>
-      <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
-        <form>
-          <label for="name" className="font-medium text-gray-900 block mb-1">
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-          />
-          <label
-            for="email"
-            className="font-medium text-gray-900 block mb-1 mt-8"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-          />
-          <label
-            for="email"
-            className="font-medium text-gray-900 block mb-1 mt-8"
-          >
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-          />
-          <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
-            Submit
-          </button>
-        </form>
-      </div>
     </Section>
   );
 };
